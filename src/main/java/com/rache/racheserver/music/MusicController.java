4ab6683620992eda35dfcc742cfbf6a8f3c0a331
LRache <2746324475@qq.com>
2024-11-22 20:59:43 +0800

    feat: modify origin cross

--- src/main/java/com/rache/racheserver/music/MusicController.java
+++ src/main/java/com/rache/racheserver/music/MusicController.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:3000"})
+@CrossOrigin(origins = {"*"})
 @RequestMapping("/music")
 public class MusicController {
 
